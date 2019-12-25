8f2ff9ac39a2c080a697c57fb4baafb2096018f4
Deepanshu Vangani <vanganideepanshu@gmail.com>
2019-12-25 16:16:05 +0530

    changed the db config to support multiple queries

--- config/database-connection.js
+++ config/database-connection.js
@@ -5,7 +5,8 @@ var pool = mysql.createPool({
     host: 'localhost',
     user: 'root',
     password: '',
-    database: 'chat-bot'
+    database: 'chat-bot',
+    multipleStatements: true
 });
 
 module.exports = pool;
