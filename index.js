d147c6f8e2df2019437459079fa50d4069435671
Deepanshu Vangani <vanganideepanshu@gmail.com>
2020-01-14 16:47:13 +0530

    added ui for login page

--- index.js
+++ index.js
@@ -7,9 +7,14 @@ app.use(express.static('views'));
 
 app.use('/admin',adminRoutes);
 
-app.use('/chat',chatRoutes);
+app.use('/chat', chatRoutes);
 
-app.get('/' , (req,res)=>{
+
+app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname + '/views/html/chatbot.html'))
+});
+
+app.get('/login', (req, res) => {
+    res.sendFile(path.join(__dirname + '/views/html/loginPage.html'))
 })
 app.listen(3000);
